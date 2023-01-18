2668c69f4e5c59426fd28c56199eb464942ca3f7
imre-gabor <imre.gabor@autsoft.hu>
2023-01-18 14:57:39 +0100

    fix imports (javax->jakarta)

--- airport/src/main/java/hu/webuni/airport/model/D.java
+++ airport/src/main/java/hu/webuni/airport/model/D.java
@@ -1,10 +1,6 @@
 package hu.webuni.airport.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
+import jakarta.persistence.Entity;
 
 @Entity
 public class D extends C {
